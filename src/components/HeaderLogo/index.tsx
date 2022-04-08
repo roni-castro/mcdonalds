bc67c8d89f2c4d4100fe1001e4c14607e8edb712
Roni Castro <roni.castro@devbase.us>
2022-04-07 23:52:24 -0300

    feat: create HeaderLogo component for navigation

---
+++ src/components/HeaderLogo/index.tsx
@@ -0,0 +1,6 @@
+import logo from '../../../assets/logo.png';
+import { LogoImage } from './styles';
+
+export default function HeaderLogo() {
+  return <LogoImage source={logo} />;
+}
---
+++ src/components/HeaderLogo/styles.ts
@@ -0,0 +1,6 @@
+import styled from 'styled-components/native';
+
+export const LogoImage = styled.Image({
+  width: 108,
+  height: 108,
+});
